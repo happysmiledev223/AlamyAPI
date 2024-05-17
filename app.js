2a08cb34dc5ae8cb24ee3b37af837dfaf0f129e3
happysmiledev223 <stress100000@outlook.com>
2024-05-17 12:27:05 -0400

    update public

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /publi c
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'tmp')));
 
 app.use('/', index);
 app.use('/shopify', shopify);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var url = require('url');
 var verifyCall = require('../tools/verify');
+const fs = require('fs');
 
 //var config = require('../config/config.json'); //testing
 /* GET home page. */
